055c23e1c4bdc47aa6d5352df6b12c14e68a9690
Leonardo Calvet <leonardofmcalvet@gmail.com>
2022-09-01 15:46:06 -0300

    Import some https

--- Imersao/src/App.java
+++ Imersao/src/App.java
@@ -1,5 +1,14 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_8tjfnzz3";
+        URI local_url = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(local_url).GET().build();
+        // client.send(, responseBodyHandler)
+
     }
 }
